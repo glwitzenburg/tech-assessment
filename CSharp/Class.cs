e66c91750967790daf44edc2915b135b4e798075
Garrett <glwitzenburg@gmail.com>
2022-06-30 04:47:41 -0500

    Pushing changes

---
+++ CSharp/Class.cs
@@ -0,0 +1,12 @@
+﻿using CSharp.Models;
+using System;
+
+namespace CSharp
+{
+    public class Class
+    {
+        
+
+        
+    }
+}
---
+++ CSharp/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+
+}
